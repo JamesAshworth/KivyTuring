d8404a343079931158eaefd3ad02d2dc7143bcd2
JamesAshworth <j.t.ashworth@warwick.ac.uk>
2015-04-01 04:11:06 +1100

    Runtime buttons
    Created buttons to step, run, pause and reset a simulation.

--- runtoolbar.py
+++ runtoolbar.py
@@ -1,14 +1,50 @@
 from kivy.app import App
+from kivy.clock import Clock
 from kivy.uix.boxlayout import BoxLayout
-from buttons import SwitchButton, Spacer
+from buttons import ExtendButton, StickyButton, SwitchButton, Spacer
 import globvars
+import logic
+
+class StepButton(ExtendButton):
+    def on_press(self):
+        logic.do_step()
+        
+class BuildButton(SwitchButton):
+    def on_press(self):
+        logic.end_simulation()
+        super(BuildButton, self).on_press()
+        
+class RunButton(StickyButton):
+    def on_press(self):
+        globvars.AllItems['running'] = True
+        logic.do_step()
+        super(RunButton, self).on_press()
+        
+class PauseButton(StickyButton):
+    def on_press(self):
+        globvars.AllItems['running'] = False
+        super(PauseButton, self).on_press()
+        self.selected(False)
+        
+class ResetButton(StickyButton):
+    def on_press(self):
+        Clock.unschedule(globvars.AllItems['movementClock'])
+        globvars.AllItems['running'] = False
+        logic.reset_simulation()
+        super(ResetButton, self).on_press()
+        self.selected(False)
             
 class RunToolbar(BoxLayout):
     def __init__(self, *args, **kwargs):
         super(RunToolbar, self).__init__(*args, **kwargs)
         self.size_hint = 1, None
         self.height = 60
-        self.add_widget(SwitchButton(mode = "build", text = "Build", direction = "right", newmode = "move", button = globvars.AllItems['move']))
+        self.add_widget(BuildButton(mode = "build", text = "Build", direction = "right", newmode = "move", button = globvars.AllItems['move']))
+        self.add_widget(Spacer())
+        self.add_widget(StepButton(text = "Step"))
+        self.add_widget(RunButton(text = "Run"))
+        self.add_widget(PauseButton(text = "Pause"))
+        self.add_widget(ResetButton(text = "Reset"))
         self.add_widget(Spacer())
 
 class PrototypeApp(App):
