8c588be118dfe84e0e1d22aaebb4f8b242090b12
JamesAshworth <j.t.ashworth@warwick.ac.uk>
2015-05-23 22:01:26 +1000

    Attempt Keyboard Fix
    Attempt a fix to emulate kivy 1.9.0 keyboard 'window resize'
    functionality.

--- main.py
+++ main.py
@@ -4,11 +4,16 @@ from kivy.app import App
 from startscreen import StartScreen
 from machinescreen import MachineScreen
 from kivy.uix.screenmanager import ScreenManager, Screen
+from kivy.uix.boxlayout import BoxLayout
+from kivy.uix.widget import Widget
+from kivy.properties import NumericProperty
+from kivy.utils import platform
+from kivy.clock import Clock
 import globvars
 
-class Application(ScreenManager):
+class _Application(ScreenManager):
     def __init__(self, *args, **kwargs):
-        super(Application, self).__init__(*args, **kwargs)
+        super(_Application, self).__init__(*args, **kwargs)
         globvars.AllItems['application'] = self
         screen = Screen(name = "menu")
         startScreen = StartScreen()
@@ -18,6 +23,29 @@ class Application(ScreenManager):
         screen = Screen(name = "machine")
         screen.add_widget(MachineScreen())
         self.add_widget(screen)
+        
+class Application(BoxLayout):
+    def __init__(self, *args, **kwargs):
+        super(Application, self).__init__(*args, **kwargs)
+        self.orientation = 'vertical'
+        self.add_widget(_Application())
+        self.keyboardSpacer = Widget(size_hint = (1, None), height = 0)
+        self.add_widget(self.keyboardSpacer)
+        self.trigger_keyboard_height = Clock.create_trigger(self._upd_kbd_height, .5)
+
+    def _upd_kbd_height(self, *kargs):
+        self.keyboardSpacer.height = self._get_kheight
+    
+    def _get_android_kheight(self):
+        global android
+        if not android:
+            import android
+        return android.get_keyboard_height()
+
+    def _get_kheight(self):
+        if platform == 'android':
+            return self._get_android_kheight()
+        return 0
         
 class TuringApp(App):
     def open_settings(self, *largs):
